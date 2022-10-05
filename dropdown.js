36c65c0f3202d9ab07a1168f724efd08cf15c80e
Ben Daniels <benbanj@hotmail.com>
2022-10-05 19:16:01 +0100

    Rework dropdown module from IIFE

--- dropdown.js
+++ dropdown.js
@@ -1,11 +1,16 @@
 const dropdown = (() => {
-  const dropdownMenus = document.querySelectorAll('.dropdown-menu');
-  console.log('I am working');
-  dropdownMenus.forEach(menu => {
-    menu.menuContent = menu.querySelector('.dropdown-content');
-    menu.addEventListener('mouseover', openMenu);
-    menu.addEventListener('mouseout', closeMenu);
-  })
+  function dropdowns() {
+    const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+    return dropdownMenus;
+  }
+
+  function setListeners() {
+    dropdowns().forEach(menu => {
+      menu.menuContent = menu.querySelector('.dropdown-content');
+      menu.addEventListener('mouseover', openMenu);
+      menu.addEventListener('mouseout', closeMenu);
+    })
+  }
 
   function openMenu() {
     this.menuContent.classList.add('open-menu');
@@ -14,4 +19,12 @@ const dropdown = (() => {
   function closeMenu(e) {
     this.menuContent.classList.remove('open-menu');
   }
+
+  function setup() {
+    setListeners();
+  }
+
+  return { setup };
 })();
+
+export { dropdown };
